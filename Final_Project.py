c9eee2426fd3f0c79c534b2dca180fd7a9f7b3b9
alexanduh <alexander.j.peng@gmail.com>
2020-11-18 17:36:20 -0800

    mongolian branch

--- Final_Project.py
+++ Final_Project.py
@@ -1,12 +1,11 @@
-
-
+import mongolian
 time_periods = ['1207']
-
 
 def time_select(time_periods):
     print(time_periods)
     years = int(input ("You are a time traveler, where do you wish to go?"))
     if years == (1207):
         print("Welcome to the Mongolian period")
+        mongolian.chose_mongolian()
 
 time_select(time_periods)
---
+++ mongolian.py
@@ -0,0 +1,6 @@
+def chose_mongolian():
+    print("You step out into the arid steppes of Inner Mongolia. You feel a cold breeze and notice colorful banners surrounding a tent city in the distance")
+    choice = input("Do you want to go to the city? y/n")
+    if(choice == "y"):
+        print("you go to the city")
+
