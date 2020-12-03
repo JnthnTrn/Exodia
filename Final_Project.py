8598d139daf266f96ade01d47d52a99a358cf974
Anrusso23 <71799934+Anrusso23@users.noreply.github.com>
2020-12-02 16:26:34 -0800

    Beginning

--- Final_Project.py
+++ Final_Project.py
@@ -1,11 +1,13 @@
 import mongolian
 time_periods = ['1207']
 
+print("After years of development, you have successfully created a time travel device. The device comes in the form of a small watch with a touchscreen. By adjusting the settings on the watch, you can travel to any time and location that you want. However, you only have enough power for a round trip, so use it wisely. For your first trip, you have 2 different time periods in mind: 1207 CE, Mongolia, and 1390 CE, England")
+
 def time_select(time_periods):
     print(time_periods)
-    years = int(input ("You are a time traveler, where do you wish to go?"))
+    years = int(input ("What time period do you want to travel to?"))
     if years == (1207):
-        print("Welcome to the Mongolian period")
+       
         mongolian.chose_mongolian()
 
 time_select(time_periods)
