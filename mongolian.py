56c74da797dca3b818338cf4892d00239337cf94
ShiHuangJacky <71676330+ShiHuangJacky@users.noreply.github.com>
2020-12-07 16:32:53 -0800

    V.2 changes
    yes

--- mongolian.py
+++ mongolian.py
@@ -7,12 +7,49 @@ def chose_mongolian():
         print("As you are walking, you get near a secluded downhill area and spot two people harassing a little girl.")
         print("You notice the girl has ears that looks like a cat, you shake this fact off and call the two dudes to get their autographs.")
         print("This gives the girl an opportunity to escape without notice. You keep in mind where she is headed.")
-        
+        print("You apologize for bothering them and for letting the girl escape and head towards where the girl was headed, the main market.")
+        print("You pass by a local pawnshop and remember that you have the autographs from the two men.")
+    choice1.2 = input("Do you wish to sell the autographs? y/n")
+        if(choice1.2 == "y"):
+            print("You talk to the store clerk.")
+            print("Hey, how much do you think these could sell for?")
+            print("I hand him the autographs.")
+            print("Clerk: No way! You have autographs from these two? They are famous for never signing anything!")
+            print("Clerk: You didn't do anything underhanded to get these did you?")
+            print("No, no, of course I didn't, uhhh I just asked them for an autograph and they gave it to me!")
+            print("Clerk: Alright, I'll believe you.")
+            print("Clerk: Considering these are very limited, these will probably run me 3 silver coins, enough to live for a week.")
+            print("You've got yourself a deal.")
+        if(choice1.2 == "n"):
+            print("The store clerk yells at you,\"Hey! what are those in your hand?"")
+
+
     if(choice == "n"):
         print("You see a strange hooded person walking away from the tent city. The person approaches you.")
         print("You get bumped into and the person falls and is unhooded.")
         print("???: Sorry, I didn't mean to hit you.")
         print("You notice that the person is a little girl with weird looking ears, akin to a cats")
+        print("You don't have to worry about it, but what's your name?")
+        print("???: Oh sorry, I forgot to introduce myself, I'm Dragar.")
+        print("The girl struggles to get up and I notice her pale, skinny legs.")
+        print("Dragar: But anyway mister, do you have any food to spare?")
+    choice2.2 = input("Yes, I do have food / No, I don't have food")
+    if(choice2.2 == "Yes I do have food"):
+        print("Dragar: Really? Can I pleeeease have some?")
+        print("You realize you dont have any food nor money.")
+        print("Actually, sorry I don't have food on me, but we can find some for sure... probably.")
+        print("Dragar: Aww, ok, follow me. I know just the spot!")
+        print("Dragar leads me to the back of a restaurant named Planders.")
+        print("Dragar: Here we are! my paradise")
+        print("Alright well, it's the best we've got I guess.")
+    if(choice2.2 == "No, I don't have food")
+    print("Sorry, I dont have any food right now.")
+    print("Dragar: Aww, ok then.")
+    print("Dragar slowly walks away")
+    print("I call out to her")
+    print("Actually even if I don't have food, follow me. We can just find some food.")
+    
+
 
 
 
