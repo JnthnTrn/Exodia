a29102705b1e84eaf4e54b7384e7f6d6d72f8b64
Anrusso23 <71799934+Anrusso23@users.noreply.github.com>
2020-12-07 16:21:25 -0800

    alternate path in progress

---
+++ alternate paths.py
@@ -0,0 +1,5 @@
+print("Drawn to the sound of running water, you make your way farther into the forest")
+print("You follow what seems to be a small, human made path, which eventually connects to the river you heard. You stop to appreciate the clean, clear water when you suddenly see something large moving through in your direction. Do you duck and hide, or do you try to get a closer look?")  
+choice = input("What do you do?try to hide/get a closer look")
+if (choice == "try to hide"):
+    print("you try to hide")
