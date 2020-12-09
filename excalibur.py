e66bb0219f460c9d250305b55dcddff900d4b769
JnthnTrn <jonathantran0106@gmail.com>
2020-12-08 16:22:37 -0800

    Changess

--- excalibur.py
+++ excalibur.py
@@ -8,16 +8,16 @@ def chose_excalibur():
         print("*You look around you, but can't seem to visually find the voice. It sounds like it's coming from a small hole in the gates*")
         print("\"???: Do you understand English boy?\"")
         choice1_1 = input ("Come in peace / Insult the guard")
-        if (choice1_1 == "Come in peace")
+        if (choice1_1 == "Come in peace"):
             print("You: Aye, I seek only peace and want no trouble")
             print("???: You're quite a ways away from safety boy!")
             print("???: Where are you from?")
 
             choice1_2 = input ("Lie / Tell the truth")
-            if (choice1_2 == "Lie")
+            if (choice1_2 == "Lie"):
                 print("You: I'm from the kingdom of Wessex.")
                 print("You: I'm ")
-            if (choice1_2 == "Tell the truth")
+            if (choice1_2 == "Tell the truth"):
                 print("You: I don't know sir.")
                 print("You: I awoken in the forest, with no memory of whom I once was.")
                 print("You: I've got no sense of who I am or where I am.")
@@ -27,15 +27,15 @@ def chose_excalibur():
 
 
 
-        if (choice1_1 == "Insult the guard")
+        if (choice1_1 == "Insult the guard"):
             print("You: Am I to be intimidated by a guard who is afraid to face his opponents directly?")
             print("???: You're willing to reinforce those words with the clash of metal then?")
             print("*You realize, you don't have a sword with you*")
 
             choice1_3 = input ("Bluff / Take back what you said")
-            if choice1_3 == ("Bluff")
+            if choice1_3 == ("Bluff"):
                 print()
-            if choice1_3 == ("Take back what you said")
+            if choice1_3 == ("Take back what you said"):
                 print()
 
 
