a933c34e09746497dabb2db1605f8f4f9a481a43
Anrusso23 <71799934+Anrusso23@users.noreply.github.com>
2020-12-12 10:39:08 -0800

    final changes
    f

--- excalibur.py
+++ excalibur.py
@@ -42,5 +42,22 @@ def chose_excalibur():
 
     if (choice == "n"):
         print("You get up, and walk towards the mysterious sound in the depths of the forest")
+    
+  
+        print("Drawn to the sound of running water, you make your way farther into the forest")
+        print("You follow what seems to be a small, human made path, which eventually connects to the river you heard. You stop to appreciate the clean, clear water when you suddenly see something large moving through in your direction. Do you hide, or do you try to get a closer look?")  
+        choice = input ("What do you do? try to hide / closer look")
+        if (choice == "try to hide"):
+            print("You dive for the nearest patch of growth that can conceal you and listen to your surroundings. Over the excited chatter of birds and the hushed sound of the wind you can faintly hear hushed voices through the dense undergrowth. After around a minute, fade out and you are left with the quiet ambience of the forest once again. Suddenly you notice the most beautiful creature that you have ever seen. You only see it for a fraction of a second, but you are immediately enchanted by it. Money,progress,time travel, none of it is as important as this creature now. You never return from the forest.")
 
+        if (choice == "closer look"):
+            print("You move towards the sounds to find what looks like a dozen hunters carrying bows marching through the forest. Cautiously, you try to signal to them without making any sudden movements. You are still unsure about whether or not they are hostile. At the sight of you, they pause, and then one of them starts shouting. Before you can react, he draws his bow and takes aim at you. Confused and scared, you run as far as you can before taking cover behind a tree. A hail of arrows fly past you. Following the arrows comes the heavy thud of footsteps as you hear the entire group charging your position.") 
+            print("Realizing that you only have one way out, you quickly activate the time travel sequence on your watch.") 
+            print("You hear the foosteps getting closer")
+            print("2...")
+            print("A second wave of arrows flies past you")
+            print("1...")
+            print("A large bearded man comes around the corner with an axe, swinging it directly at you")
+            print("0...")
+            print("You wake up in your room again, intact. You are dissapointed that your experience ended so soon, but it could have been worse")
 
